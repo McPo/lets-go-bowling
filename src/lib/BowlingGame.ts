9581aa06da480ece61007e08e92f99650748ddc2
Emmet McPoland <emcpoland@gmail.com>
2025-03-14 12:42:50 +1100

    fixed simple test

--- src/lib/BowlingGame.ts
+++ src/lib/BowlingGame.ts
@@ -3,7 +3,7 @@ export default class BowlingGame {
     private _score : number = 0;
 
     public roll(numPins:number) {
-        this._score += this._score;
+        this._score += numPins;
     }
 
     public get score() {
