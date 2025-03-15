2e4bd94a6c0924a49fcec6dab530030fc6bd5e46
Emmet McPoland <emcpoland@gmail.com>
2025-03-15 11:46:10 +1100

    starting to build out frame class, laying ground work for more complicated scoring

--- src/lib/Frame.ts
+++ src/lib/Frame.ts
@@ -1,6 +1,16 @@
 
-export type Frame = [number, number];
+export default class Frame {
+    private _rolls : number[];
+
+    constructor(rolls:number[] = []) {
+        this._rolls = rolls;
+    }
+
+    public get score() {
+        return this._rolls.reduce((sum, r) => sum+r);
+    }
+}
 
 export function calculateScore(frames:Frame[]): number {
-    return frames.flat().reduce((sum,r) => sum+r);
+    return frames.flat().reduce((sum,r) => sum+r.score, 0);
 }
--- tests/Frame.test.ts
+++ tests/Frame.test.ts
@@ -1,9 +1,9 @@
-import { calculateScore } from '@/lib/Frame';
+import Frame, { calculateScore } from '@/lib/Frame';
 
 test("Basic rolls", () => {
     expect(calculateScore([
-        [5,3],
-        [2,1]
+        new Frame([5,3]),
+        new Frame([2,1])
     ]) ).toEqual(
         5+3
         +2+1
@@ -12,8 +12,8 @@ test("Basic rolls", () => {
 
 test("Spare rolls", () => {
     expect(calculateScore([
-        [9,1],
-        [2,1]
+        new Frame([9,1]),
+        new Frame([2,1])
     ]) ).toEqual(
         9+1+2
         +2+1
