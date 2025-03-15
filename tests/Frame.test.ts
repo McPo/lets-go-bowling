cbe86ef4a1045eead81183132fb9335bbcce8144
Emmet McPoland <emcpoland@gmail.com>
2025-03-15 11:40:49 +1100

    Some test tidy and added spare test , failing

--- tests/Frame.test.ts
+++ tests/Frame.test.ts
@@ -4,5 +4,18 @@ test("Basic rolls", () => {
     expect(calculateScore([
         [5,3],
         [2,1]
-    ]) ).toBe(5+3+2+1);
+    ]) ).toEqual(
+        5+3
+        +2+1
+    );
+});
+
+test("Spare rolls", () => {
+    expect(calculateScore([
+        [9,1],
+        [2,1]
+    ]) ).toEqual(
+        9+1+2
+        +2+1
+    );
 });
