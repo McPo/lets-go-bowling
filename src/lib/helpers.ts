660cf305aa23371343644cc468ef97d6c244b6c1
Emmet McPoland <emcpoland@gmail.com>
2025-03-16 18:23:15 +1100

    ok helpers seem to be sufficent and tieded

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,10 +1,10 @@
 import Frame from "./Frame";
 
-export const sumArray = (arr: number[]) => arr.reduce((sum, r) => sum+r, 0)
+export const sumArray = (arr: number[]) => arr.reduce((sum, r) => sum+r, 0);
 
-export function calculateScore(frames:Frame[]): number {
-    const upcomingRolls =  (i:number, x:number) : number[] => frames.slice(i+1, i+x+1).map(f => f.rolls).flat().slice(0, x);
-    return frames.flat().reduce((sum,f,i) => {
+export function calculateScore(frames: Frame[]): number {
+    const upcomingRolls =  (i: number, x: number): number[] => frames.slice(i+1, i+x+1).map(f => f.rolls).flat().slice(0, x);
+    return frames.flat().reduce((sum, f, i) => {
         if (f.isStrike) return sum + f.score + sumArray(upcomingRolls(i, 2));
         else if (f.isSpare) return sum + f.score + sumArray(upcomingRolls(i, 1));
         else return sum + f.score
