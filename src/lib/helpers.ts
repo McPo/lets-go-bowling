a498ceb833a9cc47f30c5d9e8e9fd6e83c750a80
Emmet McPoland <emcpoland@gmail.com>
2025-03-16 17:50:48 +1100

    slight optimisation

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -4,7 +4,7 @@ export const sumArray = (arr: number[]) => arr.reduce((sum, r) => sum+r, 0) // n
 
 export function calculateScore(frames:Frame[]): number {
     return frames.flat().reduce((sum,f,i) => {
-        const upcomingRolls =  (x:number) : number[] => frames.slice(i+1).map(f => f.rolls).flat().slice(0, x); // needs test?
+        const upcomingRolls =  (x:number) : number[] => frames.slice(i+1, i+x+1).map(f => f.rolls).flat().slice(0, x);
 
         if (f.isStrike) return sum + f.score + sumArray(upcomingRolls(2));
         else if (f.isSpare) return sum + f.score + sumArray(upcomingRolls(1));
